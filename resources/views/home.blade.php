63e520b57be987be0045d304c9b63d73328800cc
nbn <nabin62246@gmail.com>
2019-08-06 20:46:24 +0545

    uploaded from local

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <create-post />
+        </div>
+        <div class="col-md-6 posts-container" style="height:35rem; overflow-y:scroll">
+            <all-posts />
+        </div>
+    </div>
+    @endsection
